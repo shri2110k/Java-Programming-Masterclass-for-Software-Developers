b1ad02f618ff1439326f085be4fddb04555f97c5
shrikant kachare <shri2110k@gmail.com>
2020-11-09 23:43:08 +0530

    variables

--- JavaMatserClass/src/com/udemy/masterclass/firststep/HelloWorld.java
+++ JavaMatserClass/src/com/udemy/masterclass/firststep/HelloWorld.java
@@ -3,8 +3,7 @@ package com.udemy.masterclass.firststep;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Shrikant..this is first step to learn java!!");
-
+		System.out.println("Hello Shrikant..this is first step to learn java!!");	
 	}
 
 }
---
+++ JavaMatserClass/src/com/udemy/masterclass/firststep/Variables.java
@@ -0,0 +1,18 @@
+package com.udemy.masterclass.firststep;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int num1 = 5;
+		System.out.println(num1);
+		
+		int num2 = 10 + 5;
+		System.out.println(num2);
+		
+		int num3 = (10 + 5) + (3 * 5);
+		System.out.println(num3);
+
+	}
+
+}
